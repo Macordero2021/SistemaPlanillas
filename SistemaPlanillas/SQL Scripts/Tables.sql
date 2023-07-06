945ac926e035b6153b0d108ff42a915b581ff8e7
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-07-05 23:08:54 -0600

    Creating tables and working on the Usercontroller

---
+++ SistemaPlanillas/SQL Scripts/Tables.sql
@@ -0,0 +1,99 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[id] [int] NOT NULL,
+	[name] [varchar](100) NULL,
+	[lastName] [varchar](100) NULL,
+	[email] [varchar](100) NULL,
+	[phone] [varchar](100) NULL,
+	[password] [varchar](100) NULL,
+	[fkStatus] [int] NULL,
+	[dateCreate] [date] NULL,
+	[dateUpdate] [date] NULL,
+	[fkCreateUser] [int] NULL,
+	[fkUpdateUser] [int] NULL,
+	[fkPaymentMethod] [int] NULL,
+	[Salary] [varchar](100) NULL,
+	PRIMARY KEY CLUSTERED 
+(
+
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)ON [PRIMARY]
+/****** --------------------------------------------------------------------------------------- ******/
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[id] [int] NOT NULL,
+	[name] [nchar](100) NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** --------------------------------------------------------------------------------------- ******/
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RolDepartmentUser](
+	[id] [int] NOT NULL,
+	[fkRoles] [int] NULL,
+	[fkDepartments] [int] NULL,
+	[fkUser] [int] NULL,
+ CONSTRAINT [PK_RolDepartmentUser] PRIMARY KEY CLUSTERED 
+ (
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** --------------------------------------------------------------------------------------- ******/
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Status](
+	[id] [int] NOT NULL,
+	[name] [nchar](100) NOT NULL,
+ CONSTRAINT [PK_Status] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** --------------------------------------------------------------------------------------- ******/
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PaymentMethod](
+	[id] [int] NOT NULL,
+	[name] [nchar](100) NULL
+) ON [PRIMARY]
+GO
+
+/****** --------------------------------------------------------------------------------------- ******/
+
+CREATE TABLE [dbo].[Departments](
+	[id] [int] NOT NULL,
+	[name] [nchar](100) NULL,
+ CONSTRAINT [PK_Departments] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** --------------------------------------------------------------------------------------- ******/
