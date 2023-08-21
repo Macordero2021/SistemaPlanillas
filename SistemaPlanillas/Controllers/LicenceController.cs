a84daffeb51e9baae39077ba35bfdba875f66a50
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-08-20 19:07:46 -0600

    Creating form template to edit holidays

--- SistemaPlanillas/Controllers/LicenceController.cs
+++ SistemaPlanillas/Controllers/LicenceController.cs
@@ -40,9 +40,24 @@ namespace SistemaPlanillas.Controllers
 
         public ActionResult EditUserHoliday(int idUserLogin, int idUserEdit)
         {
-            UserHolidays idEditUserHoliday = _db.UserHolidays.Where(x => x.fk_id_user == idUserEdit).FirstOrDefault();            
+            UserHolidays idEditUserHoliday = _db.UserHolidays.Where(x => x.fk_id_user == idUserEdit).FirstOrDefault();
 
-            return View();
+            //traer el email
+            Users user = _db.Users.Where(x => x.id == idUserEdit).FirstOrDefault();
+
+            // Create a view model containing all the retrieved lists and pass it to the view.
+            UserCompositeModel viewModel = new UserCompositeModel
+            {
+                User = user,
+            };
+
+            Users userModel = _db.Users.Where(x => x.id == idUserLogin).FirstOrDefault();
+            var department = _db.Departaments.Where(x => x.id == userModel.Fk_Id_Deparment).FirstOrDefault();
+            ViewBag.UserDept = department.name_departament;
+
+            ViewBag.idUserLogin = idUserLogin;
+
+            return View(viewModel);
         }
     }
 }
