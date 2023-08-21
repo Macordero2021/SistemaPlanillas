aabf5ad10f4287852bdc94ef6c15f53a881dc1fc
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-08-20 18:14:53 -0600

    Creating the new controller and view for the licences

---
+++ SistemaPlanillas/Controllers/LicenceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaPlanillas.Controllers
+{
+    public class LicenceController : Controller
+    {
+        // GET: Licence
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
