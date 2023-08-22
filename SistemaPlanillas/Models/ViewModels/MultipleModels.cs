c3a75f2ee31d403dc363c68d5f28c2ee4a7e31d8
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-08-21 23:37:50 -0600

    Created the Payment history functionality

---
+++ SistemaPlanillas/Models/ViewModels/MultipleModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaPlanillas.Models.ViewModels
+{
+    public class MultipleModels
+    {
+        public PayrollViewModel UsersWithInfo { get; set; }
+        public List<MonthlyPaymentCompositeModel> MonthlyPayments { get; set; }
+        public List<HourlyPaymentCompositeModel> HourlyPayments { get; set; }
+    }
+}
